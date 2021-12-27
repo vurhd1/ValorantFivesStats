73d52024c730911f75e20ca4e25b33fce53dd1fc
Dhruv Ramanujan <dhruv0122@gmail.com>
2021-12-27 09:16:16 -0500

    Convert the uploaded image file to openCV and display

--- valorantfivesstats.py
+++ valorantfivesstats.py
@@ -5,6 +5,9 @@ import random
 import requests
 import time
 import shutil
+import numpy as np 
+import urllib
+import cv2
 
 from dotenv import load_dotenv
 import uuid
@@ -35,6 +38,16 @@ async def testResponse(ctx, param1: str):
     await ctx.send(f"I am a bot and I am responding to \"{param1}\"")
 
 
+#Convert the uploaded image url into an OpenCV image
+def url_to_image(url):
+    r = requests.get(url, stream=True)
+    # resp = urllib.request.urlopen(url)
+    # image = np.asarray(bytearray(r.raw.read()), dtype="uint8")
+    image = cv2.imdecode(np.frombuffer(r.raw.read(), np.uint8), cv2.IMREAD_COLOR)
+    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+
 #Upload an image to be processed
 @bot.command(name="upload", help="Upload postgame score screen")
 async def uploadImage(ctx):
@@ -47,12 +60,21 @@ async def uploadImage(ctx):
         await ctx.send(errorMessage)
 
     else:
-        print(f"The image url is: {url}")
-        r = requests.get(url, stream=True)
-        imageName = "ImageUploads/valorant5v5-" + time.strftime("%Y%m%d-%H%M%S") + ".jpeg"
-        with open(imageName, "wb") as outFile:
-            print(f"Image being saved to: {imageName}")
-            shutil.copyfileobj(r.raw, outFile)
+        image = url_to_image(url)
+        print("Image converted, showing image")
+        cv2.imshow("Image", image)
+        print("imshow finished")
+        cv2.waitKey(60000)
+        cv2.destroyAllWindows()
+        # print(image)
+        # print(f"The image url is: {url}")
+        # r = requests.get(url, stream=True)
+        # imageName = "ImageUploads/valorant5v5-" + time.strftime("%Y%m%d-%H%M%S") + ".jpeg"
+        # with open(imageName, "wb") as outFile:
+        #     print(f"Image being saved to: {imageName}")
+        #     shutil.copyfileobj(r.raw, outFile)
+
+    
 
 
 bot.run(TOKEN)
