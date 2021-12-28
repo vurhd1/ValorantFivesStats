431c590d67a39ec71176552131099bb413f09988
Dhruv Ramanujan <dhruv0122@gmail.com>
2021-12-28 11:12:45 -0500

    Added image preprocessing and set up preprocessing parameters

--- valorantfivesstats.py
+++ valorantfivesstats.py
@@ -8,9 +8,10 @@ import shutil
 import numpy as np 
 import urllib
 import cv2
+from PIL import Image
+import os
 
 from dotenv import load_dotenv
-import uuid
 from discord.ext import commands
 
 #Initial Bot Settings
@@ -21,13 +22,25 @@ intents = discord.Intents.all()
 bot = commands.Bot(intents=intents, command_prefix="#")
 
 
+#Preprocessing Parameters
+preprocessingType = "thresh"
+adaptiveThreshMethod = cv2.ADAPTIVE_THRESH_MEAN_C
+threshType = cv2.THRESH_BINARY_INV
+adaptiveThreshBlockSize = 9
+adaptiveThreshConstant = 2
+
+
+def showImage(image):
+    cv2.imshow("Image", image)
+    cv2.waitKey(60000)
+    cv2.destroyAllWindows()
+
+
 #When the bot connects to a server print server and members
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
-
     print(f"{bot.user} is connected to: \"{guild.name}\"\n")
-
     members = "\n - ".join([member.name for member in guild.members])
     print(f"Guild Members:\n - {members}\n")
 
@@ -41,11 +54,16 @@ async def testResponse(ctx, param1: str):
 #Convert the uploaded image url into an OpenCV image
 def url_to_image(url):
     r = requests.get(url, stream=True)
-    # resp = urllib.request.urlopen(url)
-    # image = np.asarray(bytearray(r.raw.read()), dtype="uint8")
     image = cv2.imdecode(np.frombuffer(r.raw.read(), np.uint8), cv2.IMREAD_COLOR)
-    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
+
+
+#Convert image to grayscale and apply threshold
+def preprocessImage(image):
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if preprocessingType == "thresh":
+        grayImage = cv2.adaptiveThreshold(grayImage, 255, adaptiveThreshMethod, threshType, adaptiveThreshBlockSize, adaptiveThreshConstant)
+    return grayImage
 
 
 #Upload an image to be processed
@@ -53,28 +71,15 @@ def url_to_image(url):
 async def uploadImage(ctx):
     try:
         url = ctx.message.attachments[0].url
-
     except IndexError:
         errorMessage = "ERROR: No image attached"
         print(errorMessage)
         await ctx.send(errorMessage)
-
     else:
         image = url_to_image(url)
-        print("Image converted, showing image")
-        cv2.imshow("Image", image)
-        print("imshow finished")
-        cv2.waitKey(60000)
-        cv2.destroyAllWindows()
-        # print(image)
-        # print(f"The image url is: {url}")
-        # r = requests.get(url, stream=True)
-        # imageName = "ImageUploads/valorant5v5-" + time.strftime("%Y%m%d-%H%M%S") + ".jpeg"
-        # with open(imageName, "wb") as outFile:
-        #     print(f"Image being saved to: {imageName}")
-        #     shutil.copyfileobj(r.raw, outFile)
-
-    
+        showImage(image)
+        preprocessedImage = preprocessImage(image)
+        showImage(preprocessedImage)
 
 
 bot.run(TOKEN)
